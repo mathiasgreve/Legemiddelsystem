a775777397d480851de5f27b4bbacd5e92ad5a6f
Mathias Greve Johannessen <mathigj@ulrik.uio.no>
2024-09-09 15:21:08 +0200

    oppdatere struktur og filer

--- TestResepter.java
+++ TestResepter.java
@@ -7,11 +7,17 @@ public class TestResepter {
     // oppretter lege
     static Lege lege = new Lege("Martine");
 
+
+    // opretter pasienter
+    static Pasient pasient1 = new Pasient("Martin", "14118438491");
+    static Pasient pasient2 = new Pasient("Fride", "10118438591");
+    static Pasient pasient3 = new Pasient("Anders", "14018438421");
+
     // oppretter noen resepter
-    static ReseptMilitaer mil = new ReseptMilitaer(nark, lege, 11);
-    static ReseptP_resept pres = new ReseptP_resept(vane, lege, 6, 5);
-    static ReseptP_resept pres1 = new ReseptP_resept(nark, lege, 6, 5);
-    static ReseptBlaa blaa = new ReseptBlaa(nark1, lege, 2, 1);
+    static ReseptMilitaer mil = new ReseptMilitaer(nark, lege, pasient1);
+    static ReseptP_resept pres = new ReseptP_resept(vane, lege, pasient2, 5);
+    static ReseptP_resept pres1 = new ReseptP_resept(nark, lege, pasient2, 5);
+    static ReseptBlaa blaa = new ReseptBlaa(nark1, lege, pasient3, 1);
 
     private static void sjekk(String hva, boolean test) {
         if (!test) {
